4f1dbe4ace61d8cfe0fab96845c8392c688736cc
Aaron Fienberg <atfienberg@gmail.com>
2025-07-29 10:29:52 -0700

    0x802f: rev 1 version of 0x2f

--- hdl/mdommb_fw_rev1_top.v
+++ hdl/mdommb_fw_rev1_top.v
@@ -371,7 +371,7 @@ module top (
 `include "mDOM_scdb_hdr_bundle_inc.v"
 `include "mDOM_bsum_bundle_inc.v"
 
-localparam[15:0] FW_VNUM = 16'h2f;
+localparam[15:0] FW_VNUM = 16'h802f;
 
 // 1 for icm clock, 0 for Q_OSC
 localparam CLK_SRC = 1;
